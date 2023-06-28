b0d5ea7b4a825eb9e8376b22445b8f5ea0fc4f6b
oli.sushil987 <oli.sushil987@gmail.com>
2023-06-28 07:32:08 +0545

    chnages

--- djangoProject7/settings.py
+++ djangoProject7/settings.py
@@ -21,6 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-&=mbft4@yg70i9bmd7)!(f$_b8u%%-_1vtm1v4rmuj*7p(aes9"
+# try:
+#     SECRET_KEY= os.environ["SECRET_KEY"]
+# except KeyError as e:
+#     raise RuntimeError("Could not find a SECRET_KEY in environment") from e
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
