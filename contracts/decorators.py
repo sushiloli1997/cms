0b2764fee131ea658be4d6349b9e4c4b22c6792b
oli.sushil987 <oli.sushil987@gmail.com>
2023-06-30 19:48:26 +0545

    new changes

--- contracts/decorators.py
+++ contracts/decorators.py
@@ -3,13 +3,13 @@ from django.shortcuts import redirect
 from django.contrib import messages
 
 
-def unauthenticated_user(view_func):
-    def wrapper_func(request, *args, **kwargs):
-        if request.user.is_authenticated:
-            return redirect('dashboard')
-        else:
-            return view_func(request, * args, **kwargs)
-    return wrapper_func
+# def unauthenticated_user(view_func):
+#     def wrapper_func(request, *args, **kwargs):
+#         if request.user.is_authenticated:
+#             return redirect('dashboard')
+#         else:
+#             return view_func(request, * args, **kwargs)
+#     return wrapper_func
 
 
 
--- djangoProject7/settings.py
+++ djangoProject7/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = "django-insecure-&=mbft4@yg70i9bmd7)!(f$_b8u%%-_1vtm1v4rmuj*7p(aes9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['a211-202-166-205-50.ngrok-free.app','localhost','127.0.0.1']
+ALLOWED_HOSTS = ['202.51.1.167','localhost','127.0.0.1']
 
 
 # Application definition
