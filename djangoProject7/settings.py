f239f51ef2adcd23da8e76318af5ede6208c2508
oli.sushil987 <oli.sushil987@gmail.com>
2023-07-01 22:41:55 +0545

    changes

--- djangoProject7/settings.py
+++ djangoProject7/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = "django-insecure-&=mbft4@yg70i9bmd7)!(f$_b8u%%-_1vtm1v4rmuj*7p(aes9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS =[]
+ALLOWED_HOSTS =['202.51.1.167']
 
 # ALLOWED_HOSTS = ['202.51.1.167','127.0.0.1']
 
