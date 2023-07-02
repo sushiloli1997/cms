bcf2e0855aa779535caef806f0897784d6395e19
oli.sushil987 <oli.sushil987@gmail.com>
2023-07-02 18:53:21 +0545

    settings changes

--- djangoProject7/settings.py
+++ djangoProject7/settings.py
@@ -16,6 +16,18 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+
+#newly added 
+
+import pymysql
+
+pymysql.install_as_MySQLdb()
+
+
+
+
+
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
